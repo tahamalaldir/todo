f1d682e08990dae09790cf7d00193dd6f7e7e334
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-08-13 21:29:13 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Calculator  />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style></style>
